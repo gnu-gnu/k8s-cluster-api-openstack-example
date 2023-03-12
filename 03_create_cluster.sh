991526b42c27ccfed0dbd6afceb0351c953e7496
Geunwoo Shim <geunwoo.j.shim@gmail.com>
2023-03-13 05:35:26 +0900

    delete cluster parameter

--- 03_create_cluster.sh
+++ 03_create_cluster.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 TEMPLATE_URL="https://github.com/kubernetes-sigs/cluster-api-provider-openstack/blob/main/templates/cluster-template-external-cloud-provider-flatcar.yaml"
-CLUSTER_NAME=${1:-capi-quickstart}
 echo "applied configuration"
 export
 clusterctl generate cluster $CLUSTER_NAME --from $TEMPLATE_URL | kubectl apply -f -
